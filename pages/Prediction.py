01f3b2ca92cc3db2c6151b4d01ea0e2211ca6d27
Yuqi Zhou <157445470+yuuuqi-zhou@users.noreply.github.com>
2026-01-26 14:30:28 +0800

    Update Prediction.py

--- pages/Prediction.py
+++ pages/Prediction.py
@@ -308,9 +308,10 @@ def draw_breach_heatmap_minimal(ax, res, pred_ret, pred_vol, title="类似首日
         va="top",
         fontsize=10.5,   # 比之前略小但更干净
         bbox=dict(
-            boxstyle="round,pad=0.28",
+            boxstyle="round,pad=0.15",
+            linespacing=1.0, 
             facecolor="white",
-            alpha=0.5,   # 白底半透明，确保可读
+            alpha=0.9,   # 白底半透明，确保可读
             edgecolor="none"
         ),
     )
